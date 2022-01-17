f20c0da0be59811b2dc186432c9a9d2691a88919
Davide Villa <nyirsh@outlook.com>
2022-01-17 09:22:04 -0600

    HD Resources

---
+++ Scripts/ExtenderMat.lua
@@ -0,0 +1,49 @@
+function KTUI_ResetScripts()
+  broadcastToAll("Reloading KT UI Extender")
+  local r = 0
+  for _, obj in ipairs(getAllObjects()) do
+    if obj.hasTag('KTUIMini') then
+      obj.reload()
+      r = r+1
+    end
+  end
+  broadcastToAll("Reloaded "..r.." items")
+end
+
+function detectItemOnTop()  --casts a ball that detects all the items on top
+    local start = {self.getPosition()[1],self.getPosition()[2]+3.1,self.getPosition()[3]}
+    local hitList = Physics.cast({
+        origin       = start,
+        direction    = {0,1,0},
+        type         = 2,
+        size         = {15,15,15},
+        max_distance = 3,
+        debug        = false,
+    })
+    return hitList
+end
+
+function ExtendUI()
+  WebRequest.get("https://raw.githubusercontent.com/nyirsh/KTUI/main/Scripts/MiniatureScript.lua", function(req)
+    if req.is_error then
+      log(req.error)
+    else
+      local allTops = detectItemOnTop()
+      local somethingExtended = false
+      local script = req.text
+
+      for _,hitlist in ipairs(allTops) do
+        local object = hitlist["hit_object"]
+        if object.tag == "Figurine" then
+          somethingExtended = true
+          object.setLuaScript(script)
+          object.addTag("KTUIMini")
+        end
+      end
+
+      if somethingExtended then
+        KTUI_ResetScripts()
+      end
+    end
+  end)
+end
