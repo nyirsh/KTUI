616006fadbee4f984b54fbbaaa5285e014612118
Davide Villa <nyirsh@outlook.com>
2022-01-17 16:35:32 -0600

    Updated token scripts

--- Scripts/SingleSidedAdvancedToken.lua
+++ Scripts/SingleSidedAdvancedToken.lua
@@ -1,4 +1,4 @@
-face_settings = { name = "Token_name", url = "https://example.com/Token_name.png", removable = true, stackable = false, secret = false }
+face_settings = { name = "Token_name", url = "https://example.com/Token_name.png", removable = true, stackable = false, secret = false, equipment = false }
 
 function KTUI_GetTokenFace()
     return face_settings
